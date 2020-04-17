911369a3a34fcc819734e5a482133d822d6034c8
Ryan Canfield <ryancanfield@me.com>
2020-04-16 17:52:30 -0700

    Update to include refetching

--- README.md
+++ README.md
@@ -6,35 +6,77 @@ This implementation of a useFetch hook adds the ability to conditionally call RE
 
 - Noop State
 - Loading state
-- Error state
+- Error state including handling of HTTP error status
 - Accepts header token
 - Conditional fetch
+- Refetch
+
+## ARGS
+
+`url` string _default: null_
+
+If the url exists as an argument of the hook, the request will be made immediately
+
+`method` string _default: GET_
+
+Set the method of HTTP request you are making
+
+`body` object _default: null_
+
+The data payload of the network request. _Note: The body does not need to be stringified beforehand as it is stringified inside the hook function._
+
+`headers` object _default: null_
+
+Any HTTP headers that you'd like to add to the request. _Note: There are default headers already included in the hook listed below. Additionally, if you want to use an auth token, read below_
+```json
+{
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
+```
+
+`addHeaders` boolean _default: true_
+
+If you'd like to send a request without headers, you can pass this argument
+
+`token` string _default: null_
+
+Useful if you'd like to add your own authorization token. It uses the following format where `token` is the value of your argument
+```json
+{
+  Authorization: `Bearer ${token}`
+}
+```
 
 ## API
 
 `noop` bool
 
-return true if the fetch url has not been called
+Return true if the fetch url has not been called
 
 `loading` bool
 
-returns true if the fetch url has been set but there has not been a response
+Returns true if the fetch url has been set but there has not been a response
 
 `response` object
 
-returns when the fetch call resolves successfully
+Returns when the request resolves successfully
 
 `error` object
 
-returns if there was a problem with the fetch call
+Returns if there was a problem executing the request
 
 `setFetchUrl` function
 
-initiates the fetch call. Once the url is set the call initiates
+Initiates the fetch request if a url wasn't supplied beforehand. Once the url is set the request initiates. Accepts a _string_ url.
 
 `setFetchBody` function
 
-when updating server data this function sets the body
+When updating server data this function sets the body. Useful if you are sending user inputted data. Used in conjuction with setFetchUrl()
+
+`rerunFetch` function
+
+Initiates another request to the server. Accepts a _boolean_ argument, typically `true`, which gets reset to false after the request resolves or errors.
 
 ## Examples
 
@@ -47,8 +89,9 @@ Simple GET
 Conditional GET
 
 ```
-  const {loading, response, error, setFetchUrl} = useFetch()
+  const {loading, response, error, setFetchUrl, rerunFetch} = useFetch()
   const getData = () => setFetchUrl('https://example.com/users')
+  const getNewData = () => rerunFetch(true)
 ```
 
 Conditional POST
--- useFetch.js
+++ useFetch.js
@@ -1,73 +1,117 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from 'react';
 
+function fetchReducer(state, action) {
+  switch (action.type) {
+    case 'SET_URL':
+      return { ...state, ...{ fetchUrl: `${action.payload}` }, loading: true };
+    case 'SET_BODY':
+      return { ...state, fetchBody: action.payload };
+    case 'SET_RESPONSE':
+      return {
+        ...state,
+        ...{ response: action.payload },
+        ...{ rerun: false },
+        loading: false,
+      };
+    case 'SET_ERROR':
+      return { ...state, ...{ error: action.payload }, ...{ rerun: false }, loading: false };
+    case 'SET_RERUN':
+      return {...state, ...{ rerun: action.payload }};
+    default:
+      throw new Error();
+  }
+}
 export const useFetch = (
-  url,
-  method = "GET",
-  body = null,
-  headers = null,
-  addHeaders = true,
-  token = null
+  url= null,
+  method= 'GET',
+  body= null,
+  headers= null,
+  addHeaders= true,
+  token= null
 ) => {
-  const [fetchUrl, setFetchUrl] = useState(url);
-  const [fetchBody, setFetchBody] = useState(body);
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
+  const initialState = {
+    loading: !!url,
+    fetchUrl: url,
+    fetchBody: body,
+    response: null,
+    error: null,
+    rerun: false,
+  };
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  const setFetchUrl = (url) => {
+    dispatch({ type: 'SET_URL', payload: url });
+  };
+  const setFetchBody = (body) => {
+    dispatch({ type: 'SET_BODY', payload: body });
+  };
+  const rerunFetch = (payload) => {
+    dispatch({type: 'SET_RERUN', payload: payload })
+  };
 
   useEffect(() => {
-    if (!fetchUrl) return;
-
+    const controller = new AbortController();
+    if (!state.fetchUrl) return;
+    let cancel = false;
     (async () => {
       try {
         let options = {
           method: method,
-          credentials: "same-origin",
+          credentials: 'same-origin',
         };
-
-        if (fetchBody) {
-          options.body = fetchBody;
+        if (state.fetchBody) {
+          options.body= JSON.stringify(state.fetchBody);
         }
 
         if (addHeaders) {
+          //default headers
+          options.headers = {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          };
           if (token) {
-            options.headers = {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            };
-          } else {
-            options.headers = {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            };
+            Object.assign(options.headers, {Authorization: `Bearer ${token}`})
+          }
+          // if call supplied optional headers
+          if (headers) {
+            options.headers = Object.assign(options.headers, headers);
           }
         }
-
-        // if call supplied optional headers
-        if (headers) {
-          options.headers = Object.assign(options.headers, headers);
-        }
-
-        const res = await fetch(fetchUrl, options);
+        const res = await fetch(state.fetchUrl, options);
         if (res.ok === false) {
-          setError({ status: res.status, message: res.statusText });
+          dispatch({
+            type: 'SET_ERROR',
+            payload: { status: res.status, message: res.statusText },
+          });
         } else {
+          if (cancel) return;
           const json = await res.json();
-
-          setResponse(json);
+          dispatch({ type: 'SET_RESPONSE', payload: json });
         }
       } catch (error) {
-        setError(error);
+        if (cancel) return;
+        if (error.name === 'AbortError') {
+          dispatch({
+            type: 'SET_ERROR',
+            payload: 'Request was canceled via controller.abort',
+          });
+        } else {
+          dispatch({ type: 'SET_ERROR', payload: error });
+        }
       }
     })();
-
-    setFetchUrl(url);
-  }, [fetchUrl]);
+    return () => {
+      controller.abort();
+      cancel = true;
+    };
+  }, [state.fetchUrl, state.rerun]); // eslint-disable-line
   return {
-    noop: !fetchUrl && !response,
-    loading: fetchUrl && !response,
-    response,
-    error,
+    noop: !state.fetchUrl && !state.response,
+    loading: state.loading,
+    response: state.response,
+    error: state.error,
     setFetchUrl,
     setFetchBody,
+    rerunFetch,
   };
 };
